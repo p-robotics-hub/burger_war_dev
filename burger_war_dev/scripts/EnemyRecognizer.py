2008e1829d07896deb4f79b8e35ff3b4e9e02073
EndoNrak <endo.noriaki.64m@kyoto-u.jp>
2021-02-22 17:40:06 +0900

    feat: add calc_enemy_distance and calc_enemy_direction

--- burger_war_dev/scripts/EnemyRecognizer.py
+++ burger_war_dev/scripts/EnemyRecognizer.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 from ColorExtractor import ColorExtractor
 # print(cv2.__version__)
 
@@ -8,15 +9,21 @@ class EnemyRecognizer:
     def __init__(self, image):
         self.image = image
         self.red_image = ColorExtractor(image).extract_red()
+        self.isEnemyRecognized = self.judgeIsEnemyRecognized()
+        self.const = 222
         self.dist = None
         self.direct = None
+
     
     def get_complement_circle_info(self):
-        _, cnts, _ = cv2.findContours(self.red_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        if ((len(cnts)==0) or (len(cnts[0])<5)):
+        if self.isEnemyRecognized:
+            _, cnts, _ = cv2.findContours(self.red_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            (x, y), radius = cv2.minEnclosingCircle(cnts[0])
+            return ((x, y), radius)
+        # elif ((len(cnts)==0) or (len(cnts[0])<5)):
+        #     return None
+        else:            
             return None
-        (x, y), radius = cv2.minEnclosingCircle(cnts[0])
-        return ((x, y), radius)
         # self.drawCenterMark((x, y), radius)
         # cv2.imshow('MinEnclosingCircle', self.image)
         # cv2.imwrite("../data/after_proc_image/10_detect_red_circle.jpg", self.image)
@@ -31,13 +38,45 @@ class EnemyRecognizer:
             markerType=cv2.MARKER_CROSS,
             markerSize=10, thickness=1)
 
+    def calcDistance(self):
+        # dist_radius_list = [[7, 32],
+        #                     [10, 22],
+        #                     [15, 15],
+        #                     [20, 11],
+        #                     [25, 9]]
+        if self.isEnemyRecognized:
+            (_, _), radius = self.get_complement_circle_info()        
+            distance = self.const/radius
+            return distance  
+        else:
+            return None  
+    
+    def calcDirection(self):
+        if self.isEnemyRecognized:
+            balloon_r = 0.0325
+            center_index = np.argmax(self.red_image.sum(axis=0))
+            direct = math.degrees(math.atan((center_index-320)*balloon_r/self.const))
+            return direct 
+        else:
+            return None         
+            
+
+    def judgeIsEnemyRecognized(self):
+        if np.max(self.red_image.sum(axis=0))<5:
+            return False
+        else:
+            return True
+
 
 if __name__ == '__main__':
     import glob
     import matplotlib.pyplot as plt
-    image_list = glob.glob("burger_war_dev/data/image_raws/210*")
+    # image_list = glob.glob("burger_war_dev/data/image_distance/*")
+    image_list = glob.glob("burger_war_dev/data/image_raws/46*")
+    image_path = image_list[0]
 
-    enemy_recognizer = EnemyRecognizer(cv2.imread(image_list[0]))
-    circle_info = enemy_recognizer.get_complement_circle_info()
-    print(circle_info)
-    
+    enem_rec = EnemyRecognizer(cv2.imread(image_path))
+    # print(len(enem_rec.red_image))
+    print(image_path)
+    print(enem_rec.calcDirection())
+    print(enem_rec.calcDistance())
--- burger_war_dev/test/test_EnemyRecognizer.py
+++ burger_war_dev/test/test_EnemyRecognizer.py
@@ -10,25 +10,44 @@ from scripts.EnemyRecognizer import EnemyRecognizer
 class TestEneRec(unittest2.TestCase):
     def setUp(self):
         self.test_images = glob.glob("data/image_raws/*")
+        self.file_folder = "data/image_raws/"
+        self.test_cases = [{"file": "1.jpg", "isEnemyRecgnized": True},
+                            {"file": "10.jpg", "isEnemyRecgnized": True},
+                            {"file": "20.jpg", "isEnemyRecgnized": True},
+                            {"file": "30.jpg", "isEnemyRecgnized": True},
+                            {"file": "46.jpg", "isEnemyRecgnized": False},
+                            {"file": "57.jpg", "isEnemyRecgnized": True},
+                            {"file": "200.jpg", "isEnemyRecgnized": True},
+                            {"file": "210.jpg", "isEnemyRecgnized": False},
+                            {"file": "410.jpg", "isEnemyRecgnized": True},
+                            {"file": "940.jpg", "isEnemyRecgnized": False},
+                            {"file": "1130.jpg", "isEnemyRecgnized": True},
+                            {"file": "1240.jpg", "isEnemyRecgnized": False}]
         
     def test_get_complement_circle_info(self):
-        file_folder = "data/image_raws/"
-        test_cases = [{"file": "1.jpg", "isEnemyRecgnized": True},
-                      {"file": "10.jpg", "isEnemyRecgnized": True},
-                      {"file": "20.jpg", "isEnemyRecgnized": True},
-                      {"file": "30.jpg", "isEnemyRecgnized": True},
-                      {"file": "46.jpg", "isEnemyRecgnized": False},
-                      {"file": "57.jpg", "isEnemyRecgnized": True},
-                      {"file": "200.jpg", "isEnemyRecgnized": True},
-                      {"file": "210.jpg", "isEnemyRecgnized": False},
-                      {"file": "410.jpg", "isEnemyRecgnized": True},
-                      {"file": "940.jpg", "isEnemyRecgnized": False},
-                      {"file": "1130.jpg", "isEnemyRecgnized": True},
-                      {"file": "1240.jpg", "isEnemyRecgnized": False}]
-
-        for case in test_cases:
+        print("\ntest_get_complement_circle_info")
+        for case in self.test_cases:
             with self.subTest(file=case["file"]):
-                file_path = file_folder + case["file"]
+                file_path = self.file_folder + case["file"]
                 print(case["file"])
                 enemy_rec = EnemyRecognizer(cv2.imread(file_path))
                 self.assertEqual(bool(enemy_rec.get_complement_circle_info()), case["isEnemyRecgnized"])
+    
+    def test_calc_distance(self):
+        print("\ntest_calc_distance")
+        for case in self.test_cases:
+            with self.subTest(file=case["file"]):
+                file_path = self.file_folder + case["file"]
+                print(case["file"])
+                enemy_rec = EnemyRecognizer(cv2.imread(file_path))
+                self.assertEqual(bool(enemy_rec.calcDistance()), case["isEnemyRecgnized"])
+    
+    def test_calc_direction(self):
+        print("\ntest_calc_direction")
+        for case in self.test_cases:
+            with self.subTest(file=case["file"]):
+                file_path = self.file_folder + case["file"]
+                print(case["file"])
+                enemy_rec = EnemyRecognizer(cv2.imread(file_path))
+                self.assertEqual(bool(enemy_rec.calcDirection()), case["isEnemyRecgnized"])
+    
