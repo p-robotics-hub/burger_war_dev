74c34b3eb8fc4c82a2438eefa961f488e4c3f480
EndoNrak <spheniscushumboldtievangelion@gmail.com>
2021-02-23 14:35:22 +0900

    fix, complete: complete imageProcessor node, having EnemyRecognizer

--- burger_war_dev/scripts/ImageProcessor.py
+++ burger_war_dev/scripts/ImageProcessor.py
@@ -7,7 +7,6 @@ from burger_war_dev.msg import img_info
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-# from ColorExtractor import ColorExtractor
 from EnemyRecognizer import EnemyRecognizer
 
 
@@ -16,6 +15,7 @@ class ImageProcessor:
         self.name = name
 
         # publisher
+        self.img = None
         self.img_info_pub = rospy.Publisher('img_info', img_info, queue_size=1)
         self.img_info = img_info()
 
@@ -23,22 +23,27 @@ class ImageProcessor:
         self.image_sub = rospy.Subscriber('/image_raw', Image, self.imageCallBack)
         self.cv_bridge = CvBridge()
 
-        # self.color_extractor = ColorExtractor()
-
-
     def imageCallBack(self, data):
         print("imageCallBack")
         try:
             self.img = self.cv_bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
-        cv2.imshow("Image window", self.img)
+        enem_rec = EnemyRecognizer(self.img)
+        if enem_rec.isEnemyRecognized:
+            self.img_info.is_enemy_recognized = True
+            self.img_info.enemy_dist = int(enem_rec.calcDistance())
+            self.img_info.enemy_direct = int(enem_rec.calcDirection())
+        else:
+            self.img_info.is_enemy_recognized = False
+            self.img_info.enemy_dist = 0
+            self.img_info.enemy_direct = 0
         # self.getImgInfo()
 
-    def getImgInfo(self):
-        enem_rec = EnemyRecognizer(self.img)
-        self.img_info.enemy_dist = int(enem_rec.calcDistance())
-        self.img_info.enemy_direct = int(enem_rec.calcDirection())
+    # def getImgInfo(self):
+    #     enem_rec = EnemyRecognizer(self.img)
+    #     self.img_info.enemy_dist = int(enem_rec.calcDistance())
+    #     self.img_info.enemy_direct = int(enem_rec.calcDirection())
         # self.view_info.enemy.targer_derection = self.getEnemyTargetDirections()
         # self.view_info.wall_target.directions = self.getWallTargetDirections()
         # self.view_info.hole.direction = self.getHoleDirections()
@@ -59,9 +64,8 @@ class ImageProcessor:
         r = rospy.Rate(1) # change speed 1fps
 
         while not rospy.is_shutdown():
-            image_info = self.getImgInfo()
-            print(image_info)
-            self.img_info_pub.publish(image_info)
+            self.img_info_pub.publish(self.img_info)
+            print(self.img_info)
             r.sleep()
 
 if __name__ == '__main__':
