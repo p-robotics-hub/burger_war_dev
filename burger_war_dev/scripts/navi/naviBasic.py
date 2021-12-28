a0af52b218a1056f1595f7682e4f671e23d050f2
ikemuuu <shohei.i0206@gmail.com>
2021-12-28 23:21:33 +0900

    rename self.path variable by ikemuuu

--- burger_war_dev/scripts/navi/naviBasic.py
+++ burger_war_dev/scripts/navi/naviBasic.py
@@ -35,7 +35,7 @@ class NaviBasic():
         # self.path_header = os.environ['HOME'] + '/catkin_ws/src/burger_war_dev/burger_war_dev/scripts/navi/waypoints'
         self.path_header = os.environ['HOME'] + '/catkin_ws_robohub/src/burger_war_dev/burger_war_dev/scripts/navi/waypoints'
         self.path_waypoints = self.path_header + '/waypoints.csv'
-        self.path_waypoints_depending_on_score = self.path_header + '/waypoints.csv'
+        self.path_waypoints_depending_on_score = self.path_header + '/waypoints_depending_on_score.csv'
         self.waypoints = Waypoint(self.path_waypoints)
         
         # velocity publisher
